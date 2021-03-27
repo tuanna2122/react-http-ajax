8eb826fd64a93c4635b543384948da2edda4ccbb
tuanna2122 <tuanna2122@gmail.com>
2021-03-28 01:45:11 +0700

    prepare for routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,53 +1,22 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import axios from '../../axios';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import Posts from './Posts/Posts';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    };
-
-    componentDidMount() {
-        axios.get('/posts')
-            .then(response => {
-                const posts = response.data.slice(0, 6);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Max'
-                    };
-                });
-                this.setState({ posts: updatedPosts });
-            })
-            .catch(error => this.setState({ error: true }));
-    };
-
-    postSelectedHandler = (id) => {
-        this.setState({ selectedPostId: id });
-    };
-
     render() {
-        let posts = <p style={{ textAlign: 'center', color: 'red' }}>Something went wrong!</p>;
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post
-                    key={post.id}
-                    title={post.title}
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)} />;
-            });
-        }
-
         return (
-            <div>
-                <section className="Posts">
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                {/* <section className="Posts">
                     {posts}
                 </section>
                 <section>
@@ -55,7 +24,8 @@ class Blog extends Component {
                 </section>
                 <section>
                     <NewPost />
-                </section>
+                </section> */}
+                <Posts />
             </div>
         );
     };
