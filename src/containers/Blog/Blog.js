3cede86df24c748cd5e771a1faad1fe40d81d0aa
tuanna2122 <tuanna2122@gmail.com>
2021-03-30 23:56:30 +0700

    lazy loading

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+// import NewPost from './NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
+    state = {
+        auth: true
+    };
+
     render() {
         return (
             <div className="Blog">
@@ -41,8 +50,11 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home 1</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    {this.state.auth && <Route path="/new-post" component={AsyncNewPost} />}
                     <Route path="/posts" component={Posts} />
+                    <Route render={() => <h1>Not found!</h1>} />
+                    {/* <Redirect from="/" to="/posts" /> */}
+                    {/* <Route path="/" component={Posts} /> */}
                 </Switch>
             </div>
         );
